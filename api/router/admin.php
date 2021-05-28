84d97eaa4ab051e262fb23e8357ff2691c3c38f2
Mark <bskin0330@gmail.com>
2021-05-28 17:31:57 +0800

    Initial admin router

--- api/router/admin.php
+++ api/router/admin.php
@@ -3,5 +3,14 @@ namespace MWPCurains\Api\Router;
 
 use WP_REST_Controller;
 
-class Admin extends WP_REST_Controller {
+class Admin extends WP_REST_Controller
+{
+    protected $namespace;
+    protected $rest_base;
+
+    public function __construct()
+    {
+        $this->namespace = 'MWPCurains/v1'
+        $this->rest_base = 'customer'
+    }
 }
